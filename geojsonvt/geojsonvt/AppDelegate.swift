7988c045bb7769a7af8ecb0391c918e72cc7688a
Justin R. Miller <incanus@codesorcery.net>
2014-12-17 16:30:26 -0800

    startup fixes

--- geojsonvt/geojsonvt/AppDelegate.swift
+++ geojsonvt/geojsonvt/AppDelegate.swift
@@ -7,6 +7,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.rootViewController = ViewController()
+        self.window!.makeKeyAndVisible()
+
         return true
     }
 
